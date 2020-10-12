8a214609caaa3fc490fe9eef036ed84ac0479e76
erinkbray <69954511+erinkbray@users.noreply.github.com>
2020-10-12 00:03:40 -0500

    added a folder select to create_ui

--- src/smartsave.py
+++ src/smartsave.py
@@ -1,10 +1,52 @@
 # {folder_path}/{descriptor}_{task}_{v###}.{ext}
 
+import maya.cmds as cmds
 import logging
 from pymel.core.system import Path
 import pymel.core as pmc
+import maya.OpenMayaUI as omui
+from PySide2 import QtWidgets, QtCore
+from shiboken2 import wrapInstance
 
 log = logging.getLogger(__name__)
+
+
+def maya_main_window():
+    """returns the maya main window widget"""
+    main_window = omui.MQtUtil.mainWindow()
+    return wrapInstance(long(main_window), QtWidgets.QWidget)
+
+
+class SmartSaveUI(QtWidgets.QDialog):
+    """Smart Save UI Class"""
+
+    def __init__(self):
+        super(SmartSaveUI, self).__init__(parent=maya_main_window())
+        self.setWindowTitle("Smart Save")
+        self.setMinimumWidth(500)
+        self.setMaximumHeight(200)
+        self.setWindowFlags(self.windowFlags() ^QtCore.Qt.WindowContextHelpButtonHint)
+
+        self.create_ui()
+
+
+    def create_ui(self):
+        self.title_lbl = QtWidgets.QLabel("Smart Save")
+        self.title_lbl.setStyleSheet("font: bold 30px")
+        self.folder_lay = self._create_folder_ui()
+        self.main_lay = QtWidgets.QVBoxLayout()
+        self.main_lay.addWidget(self.title_lbl)
+        self.main_lay.addLayout(self.folder_lay)
+        self.setLayout(self.main_lay)
+
+    def _create_folder_ui(self):
+        default_folder = Path(cmds.workspace(rootDirectory=True, query=True))
+        self.folder_le = QtWidgets.QLineEdit(default_folder)
+        self.folder_browse_btn = QtWidgets.QPushButton("...")
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.folder_le)
+        layout.addWidget(self.folder_browse_btn)
+        return layout
 
 
 class SceneFile(object):

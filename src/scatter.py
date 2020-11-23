f275144a01c57a77e0ce238397970622fa4c301f
erinkbray <69954511+erinkbray@users.noreply.github.com>
2020-11-22 23:38:09 -0600

    started merging faces and vertexes code

--- src/scatter.py
+++ src/scatter.py
@@ -4,6 +4,7 @@ import random
 import maya.OpenMayaUI as omui
 from PySide2 import QtWidgets, QtCore
 from shiboken2 import wrapInstance
+import numpy as np
 
 
 def maya_main_window():
@@ -185,26 +186,31 @@ class Scatter:
 
     def scatter(self):
         selection = cmds.ls(sl=True, fl=True)
-        pattern = "{obj}.vtx[*]".format(obj=selection[1])
-        vertex_names = cmds.ls(pattern, flatten=True)
+        pattern = "{obj}.f[*]".format(obj=selection[1])
+        face_names = cmds.ls(pattern, flatten=True)
+        print(face_names)
         object_to_instance = selection[0]
 
         if cmds.objectType(object_to_instance) == "transform":
-            self.instance_objects(object_to_instance, vertex_names)
+            self.instance_objects(object_to_instance, face_names)
         else:
             log.error("Please select a transform object for your first object")
 
-    def instance_objects(self, object_to_instance, vertex_names):
-        """instances an object to each vertex, scaling and rotating it appropriately"""
-        for vertex in vertex_names:
+    def instance_objects(self, object_to_instance, target_names, vertex=False, face=False):
+        """instances an object to each target, scaling and rotating it appropriately"""
+        for target in target_names:
             if random.randint(1, 100) < self.density:
                 new_instance = cmds.instance(object_to_instance)
                 new_instance = self.scale_instance(new_instance, self.scale_min, self.scale_max)
                 new_instance = self.rotate_instance(new_instance, self.rotate_x_min, self.rotate_x_max,
                                                     self.rotate_y_min, self.rotate_y_max, self.rotate_z_min,
                                                     self.rotate_z_max)
-                position = cmds.pointPosition(vertex, w=True)
-                cmds.move(position[0], position[1], position[2], new_instance, a=True, ws=True)
+                # position = cmds.pointPosition(vertex, w=True)
+                # cmds.move(position[0], position[1], position[2], new_instance, a=True, ws=True)
+                face_center = self.get_face_center(target)
+                face_normal = self.get_face_normal(target)
+                self.move_align(target, face_normal, face_center)
+
 
     def scale_instance(self, inst, min_scale, max_scale):
         """returns a randomly scaled instance according to user preference"""
@@ -222,17 +228,13 @@ class Scatter:
         #instead of erroring out, if the user specifies a min > max, just swap them, it should work as expected
         if x_min > x_max:
             swap_arr = swap(x_min, x_max)
-            x_min = swap_arr[0]
-            x_max = swap_arr[1]
+            x_min, x_max = [swap_arr[i] for i in swap_arr]
         if y_min > y_max:
             swap_arr = swap(y_min, y_max)
-            y_min = swap_arr[0]
-            y_max = swap_arr[1]
+            y_min, y_max = [swap_arr[i] for i in swap_arr]
         if z_min > z_max:
             swap_arr = swap(z_min, z_max)
-            z_min = swap_arr[0]
-            z_max = swap_arr[1]
-
+            z_min, z_max = [swap_arr[i] for i in swap_arr]
         x = random.randint(x_min, x_max)
         y = random.randint(y_min, y_max)
         z = random.randint(z_min, z_max)
@@ -245,26 +247,27 @@ class Scatter:
     def get_face_center(self, face_name):
         """returns numpy array of face center"""
         # vector $vertexPositions[] = `xform - q - ws - t $pFaceName`;
-        ver_positions = cmds.xform(q=true, ws=true, t=face_name)  # vector $sum = << 0, 0, 0 >>;
-        sum = np.array([[0], [0], [0]])
+        cmds.select(face_name)
+        ver_positions = cmds.xform(q=True, ws=True, t=True)  # vector $sum = << 0, 0, 0 >>;
+        ver_sum = np.array([[0], [0], [0]])
         #  vector $v;
         #   for ($v in $vertexPositions){
         for v in ver_positions:
             #       $sum = $sum + $v;
-            sum = sum + v
+            ver_sum = ver_sum + v
         # }
         # vector $average;
         average = np.array()
         #  int $numVertices = size($vertexPositions);
         num_ver = ver_positions.size
         #  $average = $sum / $numVertices;
-        average = sum / num_ver
+        average = ver_sum / num_ver
         #   return $average;
         return average
 
-    # }
     #    proc vector getFaceNormal(string $pFaceName) {
     def get_face_normal(self, face_name):
+        """returns the normal vector of the face"""
         #   string $polyInfoResult[] = `polyInfo - fn $pFaceName`;
         poly_info_result = cmds.polyInfo(fn=face_name)
         #   string $stringToParse = $polyInfoResult[0];
@@ -278,20 +281,23 @@ class Scatter:
         #   vector $normal = << $x, $y, $z >>;
         normal = np.array([[x], [y], [z]])
         #    string $parentShape[] = `listRelatives - parent $pFaceName`;
-        parent_shape = cmds.listRelatives(face_name, parent=true)
+        parent_shape = cmds.listRelatives(face_name, parent=True)
         #    string $parentTransform[] = `listRelatives - parent $parentShape[0]`;
-        parent_trans = cmds.listRelatives(parent_shape[0], parent=true)
+        parent_trans = cmds.listRelatives(parent_shape[0], parent=True)
         #    float $transformMatrix[] = `xform - q - m - ws $parentTransform[0]`;
-        transform_matrix = xform(q=true, m=true, ws=parent_trans[0])
+        transform_matrix = xform(q=True, m=True, ws=parent_trans[0])
         #    vector $worldNormal = `pointMatrixMult $normal $transformMatrix`;
         world_normal = np.array(cmds.pointMatrixMult(normal, transform_matrix))
         #    vector $unitWorldNormal = unit($worldNormal);
         # make it a unit vector
         world_normal = world_normal / np.linalg.norm(world_normal)
         #    return $unitWorldNormal;
-        return world_normal  # }#proc moveAlign(string $pObjectName, vector $pNormal, vector $pPosition){
+        return world_normal
 
+
+    #proc moveAlign(string $pObjectName, vector $pNormal, vector $pPosition){
     def move_align(self, obj_name, normal, position):
+        """positions object based on a given normal and position"""
         # vector $tangent1 = unit(cross($pNormal, << 0, 1, 0 >>));
         # compute the first tangent
         tangent1 = np.cross(normal, np.array[[0], [1], [0]])
@@ -311,13 +317,20 @@ class Scatter:
         # ($pNormal.x), ($pNormal.y), ($pNormal.z), 0.0;
         # ($tangent1.x), ($tangent1.y), ($tangent1.z), 0.0;
         # ($pPosition.x), ($pPosition.y), ($pPosition.z), 1.0 >>;
-        matrix = np.array(
-            [tangent2[0], ]
-        )  # xform - ws - m
+        matrix = np.array([
+            tangent2[0], tangent2[1], tangent2[2], 0.0,
+            normal[0], normal[1], normal[2], 0.0,
+            tangent1[0], tangent1[1], tangent1[2], 0.0,
+            position[0], position[1], position[2], 0.0
+        ])
+        # xform - ws - m
 # ($m[0][0])($m[0][1]) ($m[0][2])($m[0][3])
 # ($m[1][0])($m[1][1]) ($m[1][2])($m[1][3])
 # ($m[2][0])($m[2][1]) ($m[2][2])($m[2][3])
-# ($m[3][0])($m[3][1]) ($m[3][2])($m[3][3]) $pObjectName;#}#string $selection[] = `ls - os - fl`;
+# ($m[3][0])($m[3][1]) ($m[3][2])($m[3][3])
+        cmds.select(obj_name)
+        cmds.xform(ws=True, m=matrix)
+# $pObjectName;#}#string $selection[] = `ls - os - fl`;
 # string $faceNames[] = `filterExpand - selectionMask34 - expand true $selection`;
 # string $objectToInstance = $selection[0];
 # if (`objectType $objectToInstance` == "transform")

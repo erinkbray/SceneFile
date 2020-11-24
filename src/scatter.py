a0e4cf85c25235da66391f5c5a264c4ee5e1819c
erinkbray <69954511+erinkbray@users.noreply.github.com>
2020-11-23 22:51:41 -0600

    Attempts at bug fixes, cleaned up code (comments, etc)

--- src/scatter.py
+++ src/scatter.py
@@ -37,9 +37,20 @@ class ScatterUI(QtWidgets.QDialog):
         self.main_lay.addLayout(scale_ui)
         rotate_ui = self._create_rotation_ui()
         self.main_lay.addLayout(rotate_ui)
-        self.button = QtWidgets.QPushButton("Scatter!")
-        self.main_lay.addWidget(self.button)
+        density_ui = self._create_density_ui()
+        button_ui = self._create_button_ui()
+        self.main_lay.addLayout(density_ui)
+        self.main_lay.addLayout(button_ui)
         self.setLayout(self.main_lay)
+
+    def _create_button_ui(self):
+        """creates button ui"""
+        self.vertex_button = QtWidgets.QPushButton("Scatter to vertices!")
+        self.faces_button = QtWidgets.QPushButton("Hellify! (broken scatter to faces)")
+        self.buttons_lay = QtWidgets.QHBoxLayout()
+        self.buttons_lay.addWidget(self.vertex_button)
+        self.buttons_lay.addWidget(self.faces_button)
+        return self.buttons_lay
 
     def _create_title_labels(self):
         """Creates title and instruction labels for the main UI window"""
@@ -69,6 +80,18 @@ class ScatterUI(QtWidgets.QDialog):
         h_layout.addWidget(self.max_scale_sbx)
         layout.addLayout(h_layout)
         return layout
+
+    def _create_density_ui(self):
+        """creates density UI including label and sbx"""
+        density_lbl = QtWidgets.QLabel("4. Select the amount (0-100%) of vertices/faces to scatter to")
+        self.density_sbx = QtWidgets.QSpinBox()
+        self.density_sbx.setRange(0, 101)
+        self.density_sbx.setValue(100)
+        self.density_sbx.setSingleStep(1)
+        density_lay = QtWidgets.QVBoxLayout()
+        density_lay.addWidget(density_lbl)
+        density_lay.addWidget(self.density_sbx)
+        return density_lay
 
     def _create_rotation_ui(self):
         """creates rotation UI including X, Y, and Z min/max spinboxes and labels"""
@@ -149,15 +172,27 @@ class ScatterUI(QtWidgets.QDialog):
 
     def create_connections(self):
         """Connect widget signal to slot"""
-        self.button.clicked.connect(self.launch_scatter)
+        self.vertex_button.clicked.connect(self.launch_scatter_ver)
+        self.faces_button.clicked.connect(self.launch_scatter_faces)
 
     @QtCore.Slot()
-    def launch_scatter(self):
-        """launches scatter"""
+    def launch_scatter_ver(self):
+        """launches scatter to vertices"""
         sc = Scatter(rotate_x_min=self.min_x_sbx.value(), rotate_x_max=self.max_x_sbx.value(),
                      rotate_y_min=self.min_y_sbx.value(), rotate_y_max=self.max_y_sbx.value(),
                      rotate_z_min=self.min_z_sbx.value(), rotate_z_max=self.max_z_sbx.value(),
-                     scale_min=self.min_scale_sbx.value(), scale_max=self.max_scale_sbx.value())
+                     scale_min=self.min_scale_sbx.value(), scale_max=self.max_scale_sbx.value(),
+                     density=self.density_sbx.value(), vertex=True, faces=False)
+        sc.scatter()
+
+    @QtCore.Slot()
+    def launch_scatter_faces(self):
+        """launches scatter faces"""
+        sc = Scatter(rotate_x_min=self.min_x_sbx.value(), rotate_x_max=self.max_x_sbx.value(),
+                     rotate_y_min=self.min_y_sbx.value(), rotate_y_max=self.max_y_sbx.value(),
+                     rotate_z_min=self.min_z_sbx.value(), rotate_z_max=self.max_z_sbx.value(),
+                     scale_min=self.min_scale_sbx.value(), scale_max=self.max_scale_sbx.value(),
+                     vertex=False, faces=True)
         sc.scatter()
 
 
@@ -170,9 +205,10 @@ def swap(a, b):
 
 
 class Scatter:
+    """A class for scattering copies of maya transform objects onto other objects"""
 
     def __init__(self, density=100, rotate_x_min=0, rotate_x_max=0, rotate_y_min=0, rotate_y_max=0, rotate_z_min=0,
-                 rotate_z_max=0, scale_min=1, scale_max=1):
+                 rotate_z_max=0, scale_min=1, scale_max=1, vertex=True, faces=False):
         self.density = density
         self.rotate_x_min = rotate_x_min
         self.rotate_x_max = rotate_x_max
@@ -182,32 +218,43 @@ class Scatter:
         self.rotate_z_max = rotate_z_max
         self.scale_min = scale_min
         self.scale_max = scale_max
+        self.faces = faces
+        self.vertex = vertex
 
     def scatter(self):
+        """main. Scatters the objects."""
+        #get faces or vertices
         selection = cmds.ls(sl=True, fl=True)
-        pattern = "{obj}.f[*]".format(obj=selection[1])
-        face_names = cmds.ls(pattern, flatten=True)
+        if self.faces:
+            pattern = "{obj}.f[*]".format(obj=selection[1])
+        if self.vertex:
+            pattern = "{obj}.vtx[*]".format(obj=selection[1])
+        target_names = cmds.ls(pattern, flatten=True)
+        #use density to calculate how many targets to scatter to
+        size_targets = len(target_names)
+        target_names = random.sample(target_names, int(float(size_targets) / 100 * self.density))
         object_to_instance = selection[0]
 
         if cmds.objectType(object_to_instance) == "transform":
-            self.instance_objects(object_to_instance, face_names)
+            self.instance_objects(object_to_instance, target_names)
         else:
             log.error("Please select a transform object for your first object")
 
-    def instance_objects(self, object_to_instance, target_names, vertex=False, face=False):
+    def instance_objects(self, object_to_instance, target_names):
         """instances an object to each target, scaling and rotating it appropriately"""
         for target in target_names:
-            print("uwu")
             new_instance = cmds.instance(object_to_instance)
             new_instance = self.scale_instance(new_instance, self.scale_min, self.scale_max)
             new_instance = self.rotate_instance(new_instance, self.rotate_x_min, self.rotate_x_max,
                                                 self.rotate_y_min, self.rotate_y_max, self.rotate_z_min,
                                                 self.rotate_z_max)
-            # position = cmds.pointPosition(vertex, w=True)
-            # cmds.move(position[0], position[1], position[2], new_instance, a=True, ws=True)
-            face_center = self.get_face_center(target)
-            face_normal = self.get_face_normal(target)
-            self.move_align(target, face_normal, face_center)
+            if self.faces:
+                face_center = self.get_face_center(target)
+                face_normal = self.get_face_normal(target)
+                self.move_align(target, face_normal, face_center)
+            if self.vertex:
+                position = cmds.pointPosition(target, w=True)
+                cmds.move(position[0], position[1], position[2], new_instance, a=True, ws=True)
 
     def scale_instance(self, inst, min_scale, max_scale):
         """returns a randomly scaled instance according to user preference"""
@@ -240,44 +287,36 @@ class Scatter:
         cmds.rotate(z, inst, rotateZ=True, relative=True)
         return inst
 
-    # proc vector getFaceCenter(string $pFaceName){
     def get_face_center(self, face_name):
         """returns numpy array of face center"""
-        # vector $vertexPositions[] = `xform - q - ws - t $pFaceName`;
         cmds.select(face_name)
-        ver_positions = cmds.xform(q=True, ws=True, t=True)  # vector $sum = << 0, 0, 0 >>;
+        #get all vertices in face
+        ver_positions = cmds.xform(q=True, ws=True, t=True)
         # all vector-like arrays start with v
         v_sum = [0, 0, 0]
-        #  vector $v;
-        #   for ($v in $vertexPositions){
+        #total each dimension
         for v in ver_positions:
-            #       $sum = $sum + $v;
             for i in range (0, 3):
                 v_sum[i] += v
                 v += 1
-        # }
-        # vector $average;
         v_avg = [0, 0, 0]
-        #  int $numVertices = size($vertexPositions);
+        #get avg of each dimension
         num_ver = len(ver_positions)
-        #  $average = $sum / $numVertices;
         v_avg[0] = v_sum[0] / num_ver
         v_avg[1] = v_sum[1] / num_ver
         v_avg[2] = v_sum[2] / num_ver
-        #   return $average;
         return v_avg
 
-    #    proc vector getFaceNormal(string $pFaceName) {
     def get_face_normal(self, face_name):
         """returns the unit normal vector of the face"""
         #get the face's normal vector in LOCAL coordinates
         cmds.select(face_name)
         poly_info_result = cmds.polyInfo(fn=True)
         # convert results to floats
-        items = poly_info_result[0].split(" ")
-        x = float(items[7])
-        y = float(items[8])
-        z = float(items[9])
+        items = poly_info_result[0].split()
+        x = float(items[2])
+        y = float(items[3])
+        z = float(items[4])
         v_normal = [x, y, z]
         #rest of function converts the local coordinate to world coordinates
         #get the transform matrix
@@ -286,16 +325,15 @@ class Scatter:
         #multiply transform by the local-space vector to get the world space vector
         cmds.select(parent_trans)
         transform_matrix = cmds.xform(q=True, m=True, ws=True)
-        #    vector $worldNormal = `pointMatrixMult $normal $transformMatrix`;
         v_world_normal = self.point_matrix_mult(v_normal, transform_matrix)
         # make it a unit vector
         v_world_unit = self.unitize_vector(v_world_normal)
         #    return $unitWorldNormal;
         return v_world_unit
 
-    # proc moveAlign(string $pObjectName, vector $pNormal, vector $pPosition){
     def move_align(self, obj_name, v_normal, position):
         """positions object based on a given normal and position"""
+        v_normal = self.unitize_vector(v_normal)
         # compute the first tangent
         v_tangent_1 = self.cross_mult_vector(v_normal, [0, 1, 0])
         # make it into a unit
@@ -306,50 +344,41 @@ class Scatter:
         v_tangent_2 = self.cross_mult_vector(v_normal, v_tangent_1)
         # make it a unit
         v_tangent_2 = self.unitize_vector(v_tangent_2)
-        # matrix $m[4][4] = <<
-        # ($tangent2.x), ($tangent2.y), ($tangent2.z), 0.0;
-        # ($pNormal.x), ($pNormal.y), ($pNormal.z), 0.0;
-        # ($tangent1.x), ($tangent1.y), ($tangent1.z), 0.0;
-        # ($pPosition.x), ($pPosition.y), ($pPosition.z), 1.0 >>;
         matrix = [
             v_tangent_2[0], v_tangent_2[1], v_tangent_2[2], 0.0,
             v_normal[0], v_normal[1], v_normal[2], 0.0,
             v_tangent_1[0], v_tangent_1[1], v_tangent_1[2], 0.0,
-            position[0], position[1], position[2], 0.0
+            position[0], position[1], position[2], 1.0
         ]
-        # xform - ws - m
-        # ($m[0][0])($m[0][1]) ($m[0][2])($m[0][3])
-        # ($m[1][0])($m[1][1]) ($m[1][2])($m[1][3])
-        # ($m[2][0])($m[2][1]) ($m[2][2])($m[2][3])
-        # ($m[3][0])($m[3][1]) ($m[3][2])($m[3][3])
         cmds.select(obj_name)
+        #this is one of two possibilities for the error, i think, but i can't figure it out.
+        #this command is poorly documented and i can't figure out what it should be doing
         cmds.xform(ws=True, m=matrix)
 
-    # $pObjectName;#}#string $selection[] = `ls - os - fl`;
-    # string $faceNames[] = `filterExpand - selectionMask34 - expand true $selection`;
-    # string $objectToInstance = $selection[0];
-    # if (`objectType $objectToInstance` == "transform")
-    # {
-
     def point_matrix_mult(self, v_point, matrix):
+        """multiplies a 1x3 point matrix by a 4x4 transform matrix"""
         # i'm 80% sure this is what mel's pointMatrixMult() is doing
+        #this is likely where the error is? But i can't for the life of me figure out what it is
         product = [0, 0, 0]
-        product[0] = v_point[0] * matrix[0] + v_point[1] * matrix[1] + v_point[2] * matrix[2]
-        product[1] = v_point[0] * matrix[4] + v_point[1] * matrix[5] + v_point[2] * matrix[6]
-        product[2] = v_point[0] * matrix[8] + v_point[1] * matrix[9] + v_point[2] * matrix[10]
+        product[0] = v_point[0] * matrix[0] + v_point[1] * matrix[4] + v_point[2] * matrix[8]
+        product[1] = v_point[0] * matrix[1] + v_point[1] * matrix[5] + v_point[2] * matrix[9]
+        product[2] = v_point[0] * matrix[2] + v_point[1] * matrix[6] + v_point[2] * matrix[10]
         return product
 
     def unitize_vector(self, vector):
-        print(vector)
-        print("unitized!")
+        """returns the unit form of a vector"""
         mag = self.mag_vector(vector)
         length = len(vector)
-        for c in range (0,length):
-            vector[c] = vector[c] / mag
-        print(vector)
+        if mag == 0:
+           vector = (1, 0, 0)
+           mag = 1
+        else:
+            for c in range (0, length):
+                vector[c] = vector[c] / mag
         return vector
 
     def cross_mult_vector(self, vector_1, vector_2):
+        """cross multiplies two vectors and returns the result"""
         cross = [0, 0, 0]
         cross[0] = vector_1[1] * vector_2[2] - vector_1[2] * vector_2[1]
         cross[1] = vector_1[2] * vector_2[0] - vector_1[0] * vector_2[2]
@@ -357,6 +386,7 @@ class Scatter:
         return cross
 
     def mag_vector(self, vector):
+        """returns the magnitude of a vector"""
         total = 0
         for c in vector:
             total += c * c
